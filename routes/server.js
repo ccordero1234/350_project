2e7e03c4805f677f419105b11c00833c29bf3c2d
ccordero1234 <cesar.cordero414@myci.csuci.edu>
2024-04-15 23:14:34 -0700

    signing in

---
+++ routes/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+const staticRoute = require("./static")
+const firebaseRoute = require("./firebase")
+const rootRoute = require("./root")
+
+app.use("/static", staticRoute);
+app.use("/firebase", firebaseRoute);
+app.use("/", rootRoute);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
